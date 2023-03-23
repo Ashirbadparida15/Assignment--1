eb6d83967a04b1907ebd7c152d88048c8d2bcd93
Ashirbad Parida <ashirbadparida15@gmail.com>
2023-03-23 11:56:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-fst=input('Enter first name:')
-lst=input('Enter second name:')
-full=fst+' '+lst
-reverse=full[::-1]
-print(reverse)
+for i in range (2000,3201):
+    if i%7==0 and i %5!=0:
+        print(i , end=',')
